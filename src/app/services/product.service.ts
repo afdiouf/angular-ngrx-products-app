b4bd3ae95c0b15668835b3dfcf5205fecd965a26
Diouf <64045062+afdiouf@users.noreply.github.com>
2022-05-12 08:37:16 +0200

    Update product.service.ts

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,8 +1,3 @@
-/**
- * @author : Mohamed YOUSSFI, med@youssfi.net,
- * ENSET Mohammedia, Université Hassan II de Casablanca
- *
- */
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
